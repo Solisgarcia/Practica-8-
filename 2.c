ce1ece620aafbf22f0c3892da59904caf6bfa411
Solisgarcia <126632227+Solisgarcia@users.noreply.github.com>
2024-10-13 19:31:16 -0600

    Update 2.c

--- 2.c
+++ 2.c
@@ -1,8 +1,6 @@
 /*Solis Garcia;Luis Manuel*/
-
 #include <stdio.h>
 #include <stdlib.h>
-
 
 int exp_for(int a, int b)
 {
@@ -14,7 +12,7 @@ int exp_for(int a, int b)
         res = res * a;
     }
 
-    return(res);
+    return res;
 }
 
 int exp_while(int a, int b)
@@ -28,25 +26,34 @@ int exp_while(int a, int b)
         i = i + 1;
     }
 
-    return(res);
+    return res;
 }
 
 int main()
 {
     int a, b, res;
-    
-    printf("Dame la base: ");
-    scanf("%d", &a);
-    printf("Dame el exponente: ");
-    scanf("%d", &b);
+    char opcion;
 
-    printf("\n Exponenciacion con for");
-    res = exp_for(a, b);
-    printf("El resultado es: %d\n", res);
+    do {
+        printf("Dame la base: ");
+        scanf("%d", &a);
+        printf("Dame el exponente: ");
+        scanf("%d", &b);
 
-    printf("\n Exponenciacion con for");
-    res = exp_while(a, b);
-    printf("El resultado es: %d\n", res);
+        printf("\n Exponenciacion con for");
+        res = exp_for(a, b);
+        printf("El resultado es: %d\n", res);
+
+        printf("\n Exponenciacion con while");
+        res = exp_while(a, b);
+        printf("El resultado es: %d\n", res);
+
+        // Pregunta al usuario si desea repetir la operación
+        printf("\n¿Quieres calcular otra exponenciación? (s/n): ");
+        scanf(" %c", &opcion);
+
+    } while(opcion == 's' || opcion == 'S');  // Repite si el usuario elige 's' o 'S'
 
     return 0;
 }
+
